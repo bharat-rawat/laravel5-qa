61294f5d6813a5ca1966c683a3374224a2cce9f4
bharat <BharatRawat.in@gmail.com>
2019-12-06 16:18:31 +0530

    Added user name and created date in the question page

--- app/Question.php
+++ app/Question.php
@@ -4,15 +4,25 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\User;
+use Carbon\Carbon;
 
 class Question extends Model
 {
     protected $fillable = ['title','body'];
+
     public function user(){
         return $this->belongsTo(User::class);
     }
     public function setTitleAttribute($value){
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = str_slug($value);
+    }
+
+    public function getUrlAttribute(){
+        return route('question.show',$this->id);
+    }
+
+    public function getCreatedDateAttribute(){
+        return $this->created_at->diffForHumans();
     }
 }
