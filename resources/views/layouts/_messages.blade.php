5262af18c6cd5ddb43b746ebc4abd8fb2a38cf24
bharat <BharatRawat.in@gmail.com>
2019-12-07 00:36:08 +0530

    aded

---
+++ resources/views/layouts/_messages.blade.php
@@ -0,0 +1,12 @@
+@if(session('success'))
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    <strong> Success !</strong> {{session('success')}}
+    </div>
+    
+    <script>
+      $(".alert").alert();
+    </script>
+@endif
--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -18,25 +18,36 @@
                 <form action="{{route('question.store')}}" method="POST">
                     @csrf
                     <div class="form-group">
-                        <label for="question-title">Title of your question</label>
-                        <input type="text" name="question-title" id="question-title" class="form-control {{$errors->has('question-title')?'is-invalid':''}}">
-                        @if($errors->has('question-title'))
+                        <label for="title">Title of your question</label>
+                        <input 
+                            type="text" 
+                            name="title" 
+                            id="title" 
+                            value="{{old('title')}}"
+                            class="form-control {{$errors->has('title')?'is-invalid':''}}">
+                        @if($errors->has('title'))
                             <div class="invalid-feedback">
-                                <strong>{{$errors->first('question-title')}}</strong>
+                                <strong>{{$errors->first('title')}}</strong>
                             </div>
                         @endif
                     </div>
                     <div class="form-group">
-                        <label for="question-body">Ask your question</label>
-                        <textarea name="question-body" id="question-body" rows="10" class="form-control {{$errors->has('question-body')?'is-invalid':''}}"></textarea>
-                        @if($errors->has('question-body'))
+                        <label for="body">Ask your question</label>
+                        <textarea 
+                            name="body" 
+                            id="body" 
+                            rows="10" 
+                            class="form-control {{$errors->has('body')?'is-invalid':''}}">
+                            {{old('body')}}
+                        </textarea>
+                        @if($errors->has('body'))
                             <div class="invalid-feedback">
-                                <strong>{{$errors->first('question-body')}}</strong>
+                                <strong>{{$errors->first('body')}}</strong>
                             </div>
                         @endif
                     </div>
                     
-                    <button type="submit" name="submit" id="submit" class="btn btn-outline-secondary btn-lg">Submit this question</button>
+                    <button type="submit" name="submit" value="submit" id="submit" class="btn btn-outline-secondary btn-lg">Submit this question</button>
                 </form>
                 </div>
                 
